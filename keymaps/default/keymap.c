5cdc5ddd1c6f732e5f3dd115b42555c1f665ee93
Phusck <Phusck@gmail.com>
2023-04-26 09:33:41 +0200

    Kayma

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -3,13 +3,29 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT(
     KC_U, KC_H, KC_I, KC_T, KC_J, KC_V, KC_G,
-    KC_O, KC_N, KC_E, KC_S, KC_P, KC_M, KC_B,
+    KC_O, KC_N, KC_E, KC_S, KC_P, TG(2), KC_B,
     KC_C, KC_L, KC_A, KC_R, KC_NO, KC_NO, KC_Q,
-    KC_SPACE, KC_W, KC_X, KC_BSPC, MO(1), KC_ENT, KC_ESC,
-    KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7,
+    KC_SPACE, KC_W, KC_TAB, KC_BSPC, MO(1), KC_ENT, KC_ESC,
+    KC_LSFT, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7,
     KC_8, KC_9, KC_0, KC_Z, KC_EQUAL, KC_Y, KC_GRV
   ),
     [1] = LAYOUT(
+    KC_SCLN, KC_G, KC_F, KC_D, KC_E, KC_W, KC_G,
+    KC_QUOT, KC_B, KC_D, KC_Y, KC_L, TG(3), KC_N,
+    KC_LBRC, KC_P, KC_M, KC_K, KC_Y, KC_X, KC_U,
+    KC_V, KC_W, KC_X, KC_Y, KC_Z, KC_1, KC_2,
+    KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9,
+    KC_0, KC_EQUAL, KC_GRV, KC_BSPC, KC_TRNS, KC_ENT, KC_ESC
+  ),
+      [2] = LAYOUT(
+    KC_SCLN, KC_1, KC_4, KC_7, KC_E, KC_W, KC_G,
+    KC_QUOT, KC_2, KC_5, KC_8, KC_L, TG(0), KC_N,
+    KC_LBRC, KC_3, KC_6, KC_9, KC_Y, KC_X, KC_U,
+    KC_V, KC_W, KC_X, KC_Y, KC_Z, KC_1, KC_2,
+    KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9,
+    KC_0, KC_EQUAL, KC_GRV, KC_BSPC, KC_TRNS, KC_ENT, KC_ESC
+  ),
+      [3] = LAYOUT(
     KC_SCLN, KC_G, KC_F, KC_D, KC_E, KC_W, KC_G,
     KC_QUOT, KC_B, KC_D, KC_Y, KC_L, KC_M, KC_N,
     KC_LBRC, KC_P, KC_M, KC_K, KC_Y, KC_X, KC_U,
